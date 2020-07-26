02c9f91d94f4af8407a89f080adacd853cc33bc5
ClementPan <zzz2999582@gmail.com>
2020-07-27 00:13:12 +0800

    feat: add delete feature

--- app.js
+++ app.js
@@ -115,6 +115,16 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// remove 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 // starting the server
 app.listen(port, () => {
   console.log(`The restaurants server is working on http://localhost:${port}`)
