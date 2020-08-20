17574d8879732385dfd710b59945ff1c4fa04733
ClementPan <zzz2999582@gmail.com>
2020-08-20 11:23:23 +0800

    fix: modify show.hbs UI

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,3 +2,20 @@
 
 
 // 確認 new.hbs input 資料完整，資料型態正確。
+
+
+// 監聽刪除鍵，若被點選跳出 alert
+const deleteConfirm = function () {
+  if (!confirm('確定刪除本餐廳資料嗎？')) {
+    event.preventDefault()
+  }
+}
+
+
+
+const main = document.querySelector('main')
+main.addEventListener('click', () => {
+  if (event.target.classList.contains('delete')) {
+    deleteConfirm()
+  }
+})
